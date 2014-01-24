71c8d8dbfe8337d371eb48df46f27a8bd939c2b4
Macklin Underdown <macklin@detroitlabs.com>
2014-01-24 12:08:24 -0500

    added MainActivity file and test

---
+++ SITC/src/main/java/nu/mackli/sitc/activities/MainActivity.java
@@ -0,0 +1,12 @@
+package nu.mackli.sitc.activities;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+/**
+ * Created by macklinu on 1/24/14.
+ */
+@EActivity
+public class MainActivity extends Activity {
+}
---
+++ SITC/src/test/java/nu/mackli/sitc/activities/MainActivityTest.java
@@ -0,0 +1,11 @@
+package nu.mackli.sitc.activities;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+/**
+ * Created by macklinu on 1/24/14.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+}
