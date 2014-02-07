93aaf6d0f1eb99aa03253fa4f4bd3c7e2d2a8a5b
Macklin Underdown <macklin@detroitlabs.com>
2014-02-07 15:47:54 -0500

    created FormValidationInterface

---
+++ SITC/src/main/java/nu/mackli/sitc/interfaces/FormValidationInterface.java
@@ -0,0 +1,9 @@
+package nu.mackli.sitc.interfaces;
+
+/**
+ * Created by macklinu on 2/7/14.
+ */
+public interface FormValidationInterface {
+    public void onFormValidationSuccess();
+    public void onFormValidationError(String errorMessage);
+}
