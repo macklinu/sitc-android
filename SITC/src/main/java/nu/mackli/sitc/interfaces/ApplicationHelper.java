30bad216f6ee34bd04eb72f24cc0ce09f59fdb9a
Macklin Underdown <macklin@detroitlabs.com>
2014-02-09 16:37:20 -0500

    created ApplicationHelper

---
+++ SITC/src/main/java/nu/mackli/sitc/interfaces/ApplicationHelper.java
@@ -0,0 +1,9 @@
+package nu.mackli.sitc.interfaces;
+
+/**
+ * Created by macklinu on 2/9/14.
+ */
+public interface ApplicationHelper {
+    public long getCurrentTimeMillis();
+    public boolean isDebug();
+}
