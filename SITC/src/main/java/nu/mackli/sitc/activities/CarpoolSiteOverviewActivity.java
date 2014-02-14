a37d6ab9ca0b5bb9ec00e0696c6b85a8a6b6af32
Macklin Underdown <macklin@detroitlabs.com>
2014-02-14 13:32:56 -0500

    created CarpoolSiteOverviewActivity with layout

---
+++ SITC/src/main/java/nu/mackli/sitc/activities/CarpoolSiteOverviewActivity.java
@@ -0,0 +1,12 @@
+package nu.mackli.sitc.activities;
+
+import org.androidannotations.annotations.EActivity;
+
+import nu.mackli.sitc.R;
+
+/**
+ * Created by macklinu on 2/14/14.
+ */
+@EActivity(R.layout.activity_carpool_site_overview)
+public class CarpoolSiteOverviewActivity {
+}
