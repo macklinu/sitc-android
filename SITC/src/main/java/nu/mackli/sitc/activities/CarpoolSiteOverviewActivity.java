d459bb80029c8c9a49236769912b593f9bdb5e10
Macklin Underdown <macklin@detroitlabs.com>
2014-02-14 13:35:27 -0500

    forgot to extend the BaseActivity

--- SITC/src/main/java/nu/mackli/sitc/activities/CarpoolSiteOverviewActivity.java
+++ SITC/src/main/java/nu/mackli/sitc/activities/CarpoolSiteOverviewActivity.java
@@ -8,5 +8,5 @@ import nu.mackli.sitc.R;
  * Created by macklinu on 2/14/14.
  */
 @EActivity(R.layout.activity_carpool_site_overview)
-public class CarpoolSiteOverviewActivity {
+public class CarpoolSiteOverviewActivity extends BaseActivity {
 }
