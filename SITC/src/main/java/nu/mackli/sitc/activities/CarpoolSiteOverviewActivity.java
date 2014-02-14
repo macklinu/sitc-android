d7d9d4fd0b09d8580d616b74773fe2ee60971719
Macklin Underdown <macklin@detroitlabs.com>
2014-02-14 13:42:19 -0500

    query for the carpool sites

--- SITC/src/main/java/nu/mackli/sitc/activities/CarpoolSiteOverviewActivity.java
+++ SITC/src/main/java/nu/mackli/sitc/activities/CarpoolSiteOverviewActivity.java
@@ -1,11 +1,19 @@
 package nu.mackli.sitc.activities;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.FragmentById;
 
+import java.util.List;
+
 import nu.mackli.sitc.R;
+import nu.mackli.sitc.models.CarpoolSite;
 
 /**
  * Created by macklinu on 2/14/14.
@@ -14,4 +22,24 @@ import nu.mackli.sitc.R;
 public class CarpoolSiteOverviewActivity extends BaseActivity {
 
     @FragmentById MapFragment mapFragment;
+
+    private GoogleMap map;
+    private List<CarpoolSite> carpoolSites;
+
+    @AfterViews
+    public void onAfterViews() {
+        map = mapFragment.getMap();
+
+        ParseQuery<CarpoolSite> query = ParseQuery.getQuery(CarpoolSite.class);
+        query.findInBackground(new FindCallback<CarpoolSite>() {
+            @Override
+            public void done(List<CarpoolSite> carpoolSites, ParseException e) {
+                setCarpoolSites(carpoolSites);
+            }
+        });
+    }
+
+    public void setCarpoolSites(List<CarpoolSite> carpoolSites) {
+        this.carpoolSites = carpoolSites;
+    }
 }
