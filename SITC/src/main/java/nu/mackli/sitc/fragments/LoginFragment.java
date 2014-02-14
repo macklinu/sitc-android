84ba6cb15f2786f6aed00c3e586142ba8bd0ea21
Macklin Underdown <macklin@detroitlabs.com>
2014-02-14 13:43:09 -0500

    carpool button on login goes to this activity now

--- SITC/src/main/java/nu/mackli/sitc/fragments/LoginFragment.java
+++ SITC/src/main/java/nu/mackli/sitc/fragments/LoginFragment.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import nu.mackli.sitc.R;
 import nu.mackli.sitc.activities.CarpoolActivity_;
+import nu.mackli.sitc.activities.CarpoolSiteOverviewActivity_;
 import nu.mackli.sitc.activities.LoginActivity;
 import nu.mackli.sitc.activities.RegistrationActivity_;
 
@@ -40,7 +41,7 @@ public class LoginFragment extends BaseFragment {
     @Click
     public void carpoolButton() {
         // go to carpool activity
-        CarpoolActivity_
+        CarpoolSiteOverviewActivity_
                 .intent(getActivity())
                 .start();
     }
