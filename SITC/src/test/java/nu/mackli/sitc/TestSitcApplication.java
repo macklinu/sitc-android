6e7865e108279d0e41e7c788fa7884db39bdf9c4
Macklin Underdown <macklin@detroitlabs.com>
2014-02-16 10:58:15 -0500

    upgraded gradle for testing

---
+++ SITC/src/test/java/nu/mackli/sitc/TestSitcApplication.java
@@ -0,0 +1,7 @@
+package nu.mackli.sitc;
+
+/**
+ * Created by macklinu on 2/16/14.
+ */
+public class TestSitcApplication {
+}
---
+++ SITC/src/test/java/nu/mackli/sitc/utils/GoogleMapsUtilTest.java
@@ -0,0 +1,7 @@
+package nu.mackli.sitc.utils;
+
+/**
+ * Created by macklinu on 2/14/14.
+ */
+public class GoogleMapsUtilTest {
+}
