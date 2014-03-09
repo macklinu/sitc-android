af12ab1e2065caf7360c4bb85823e5665b8e3c88
Macklin Underdown <macklin@detroitlabs.com>
2014-03-09 10:30:17 -0400

    created role fragment

---
+++ SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationRoleFragment.java
@@ -0,0 +1,12 @@
+package nu.mackli.sitc.fragments;
+
+import android.support.v4.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+import nu.mackli.sitc.R;
+
+@EFragment(R.layout.fragment_registration_role)
+public class RegistrationRoleFragment extends Fragment {
+
+}
