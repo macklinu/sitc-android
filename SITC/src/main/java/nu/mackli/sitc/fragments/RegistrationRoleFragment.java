b9e6a6e2bcdd867c0d96f6c20ac7f258667282dd
Macklin Underdown <macklin@detroitlabs.com>
2014-03-09 11:17:47 -0400

    attempt to query the crew list

--- SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationRoleFragment.java
+++ SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationRoleFragment.java
@@ -1,13 +1,43 @@
 package nu.mackli.sitc.fragments;
 
 import android.support.v4.app.Fragment;
+import android.util.Log;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EFragment;
+
+import java.util.List;
 
 import nu.mackli.sitc.R;
 
 @EFragment(R.layout.fragment_registration_role)
 public class RegistrationRoleFragment extends Fragment {
     public static final String FRAGMENT_TAG = "registrationRoleFragment";
+
+    @AfterViews
+    public void afterViews() {
+        ParseUser parseUser = ParseUser.getCurrentUser();
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("ListCrew");
+        query.whereEqualTo("email", parseUser.getEmail());
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> crewList, ParseException e) {
+                if (e == null) {
+                    int size = crewList.size();
+                    Log.d("crew list", "Retrieved " + size + " crew members");
+                    if (size > 0) {
+                        Log.d("crew list", crewList.get(0).getString("email"));
+                    }
+                } else {
+                    Log.d("crew list", "Error: " + e.getMessage());
+                }
+            }
+        });
+    }
 
 }
--- SITC/src/main/java/nu/mackli/sitc/interfaces/RegistrationFragmentContract.java
+++ SITC/src/main/java/nu/mackli/sitc/interfaces/RegistrationFragmentContract.java
@@ -2,8 +2,6 @@ package nu.mackli.sitc.interfaces;
 
 import android.support.v4.app.Fragment;
 
-import nu.mackli.sitc.fragments.base.ContractFragment;
-
 /**
  * Created by macklinu on 3/9/14.
  */
