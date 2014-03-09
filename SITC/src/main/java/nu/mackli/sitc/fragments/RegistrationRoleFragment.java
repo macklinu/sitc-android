ae0f66066ae14ee8a30ab0be852746d00fb068dc
Macklin Underdown <macklin@detroitlabs.com>
2014-03-09 11:44:16 -0400

    adding registration role view and adjusting fragment logic

--- SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationRoleFragment.java
+++ SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationRoleFragment.java
@@ -1,7 +1,7 @@
 package nu.mackli.sitc.fragments;
 
-import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -9,35 +9,42 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
-import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
 import java.util.List;
 
 import nu.mackli.sitc.R;
+import nu.mackli.sitc.fragments.base.ContractFragment;
+import nu.mackli.sitc.interfaces.RegistrationFragmentContract;
 
 @EFragment(R.layout.fragment_registration_role)
-public class RegistrationRoleFragment extends Fragment {
+public class RegistrationRoleFragment extends ContractFragment<RegistrationFragmentContract> implements FindCallback<ParseObject> {
     public static final String FRAGMENT_TAG = "registrationRoleFragment";
 
-    @AfterViews
-    public void afterViews() {
+    @Click
+    public void crewButton() {
         ParseUser parseUser = ParseUser.getCurrentUser();
         ParseQuery<ParseObject> query = ParseQuery.getQuery("ListCrew");
         query.whereEqualTo("email", parseUser.getEmail());
-        query.findInBackground(new FindCallback<ParseObject>() {
-            public void done(List<ParseObject> crewList, ParseException e) {
-                if (e == null) {
-                    int size = crewList.size();
-                    Log.d("crew list", "Retrieved " + size + " crew members");
-                    if (size > 0) {
-                        Log.d("crew list", crewList.get(0).getString("email"));
-                    }
-                } else {
-                    Log.d("crew list", "Error: " + e.getMessage());
-                }
-            }
-        });
+        query.findInBackground(this);
     }
 
+    @Click
+    public void volunteerButton() {
+        Toast.makeText(getActivity(), "Continue with volunteer sign up flow", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void done(List<ParseObject> crewList, ParseException e) {
+        if (e == null) {
+            if (crewList.size() > 0) {
+                Toast.makeText(getActivity(), "You are a crew member!", Toast.LENGTH_SHORT).show();
+            } else {
+                // show dialog fragment
+            }
+        } else {
+            Log.d("crew list", "Error: " + e.getMessage());
+        }
+    }
 }
