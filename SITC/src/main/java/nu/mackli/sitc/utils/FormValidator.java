17a63ad81235d75c3312daddf379b2f913b4b2cf
Macklin Underdown <macklin@detroitlabs.com>
2014-03-12 22:25:51 -0400

    form stuff isn't working yet

--- SITC/src/main/java/nu/mackli/sitc/utils/FormValidator.java
+++ SITC/src/main/java/nu/mackli/sitc/utils/FormValidator.java
@@ -1,7 +1,33 @@
 package nu.mackli.sitc.utils;
 
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by macklinu on 3/9/14.
  */
 public class FormValidator {
+    ArrayList<Boolean> checks;
+    String errorMessage;
+
+    public FormValidator() {
+        checks = new ArrayList<Boolean>();
+    }
+
+    public FormValidator name(TextView textView) {
+        if (textView.getText() != null) {
+            checks.add(true);
+        } else {
+            checks.add(false);
+        }
+        return this;
+    }
+
+    public boolean validate() {
+        for (Boolean b : checks) {
+            if (!b) return false;
+        }
+        return true;
+    }
 }
--- SITC/src/test/java/nu/mackli/sitc/utils/FormValidatorTest.java
+++ SITC/src/test/java/nu/mackli/sitc/utils/FormValidatorTest.java
@@ -1,13 +1,35 @@
 package nu.mackli.sitc.utils;
 
+import android.widget.EditText;
+
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
 
 /**
  * Created by macklinu on 3/9/14.
  */
+@RunWith(RobolectricTestRunner.class)
 public class FormValidatorTest {
+    private EditText editText;
+
     @Before
     public void setUp() throws Exception {
+        editText = new EditText(Robolectric.application);
+    }
 
+    @Test
+    public void nullNameInputIsInvalid() throws Exception {
+        editText.setText(null);
+        boolean isValid = new FormValidator()
+                .name(editText)
+                .validate();
+        Assert.assertTrue(isValid);
     }
 }
