83ff7bde84c56b1ac4f06aff4a5063e7a0f12dbc
Macklin Underdown <macklin@detroitlabs.com>
2014-03-14 17:23:30 -0400

    InfoFragment form is now validated

--- SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationInfoFragment.java
+++ SITC/src/main/java/nu/mackli/sitc/fragments/RegistrationInfoFragment.java
@@ -4,6 +4,7 @@ import android.telephony.PhoneNumberUtils;
 import android.text.Editable;
 import android.view.View;
 import android.view.animation.Animation;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.devspark.appmsg.AppMsg;
@@ -11,7 +12,11 @@ import com.doomonafireball.betterpickers.datepicker.DatePickerBuilder;
 import com.doomonafireball.betterpickers.datepicker.DatePickerDialogFragment;
 import com.mobsandgeeks.saripaar.Rule;
 import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.Password;
+import com.mobsandgeeks.saripaar.annotation.Regex;
 import com.mobsandgeeks.saripaar.annotation.Required;
+import com.mobsandgeeks.saripaar.annotation.TextRule;
 
 import org.androidannotations.annotations.AfterTextChange;
 import org.androidannotations.annotations.AfterViews;
@@ -24,6 +29,7 @@ import org.androidannotations.annotations.res.AnimationRes;
 import nu.mackli.sitc.R;
 import nu.mackli.sitc.fragments.base.ContractFragment;
 import nu.mackli.sitc.interfaces.RegistrationFragmentContract;
+import nu.mackli.sitc.utils.FormRegex;
 
 /**
  * Created by macklinu on 1/26/14.
@@ -33,41 +39,55 @@ public class RegistrationInfoFragment extends ContractFragment<RegistrationFragm
         implements DatePickerDialogFragment.DatePickerDialogHandler, Validator.ValidationListener {
     public static final String FRAGMENT_TAG = "registrationFragment";
 
-    @Required(order = 0, message = "Give me a fucking first name")
+    @Required(order = 0, messageResId = R.string.error_first_name)
     @ViewById
     EditText firstNameInput;
 
-    @Required(order = 1, message = "Give me a fucking last name")
+    @Required(order = 1, messageResId = R.string.error_last_name)
     @ViewById
     EditText lastNameInput;
 
-    @Required(order = 2, message = "Give me a fucking date of birth")
+    @Required(order = 2, messageResId = R.string.error_date_of_birth)
     @ViewById
     EditText dobInput;
 
+    @Required(order = 3, messageResId = R.string.error_phone)
+    @Regex(order = 4, pattern = FormRegex.PHONE, messageResId = R.string.error_phone_valid)
     @ViewById
     EditText phoneInput;
 
+    @Required(order = 5, messageResId = R.string.error_username)
     @ViewById
     EditText usernameInput;
 
-    @Required(order = 3, message = "Give me a valid fucking email")
+    @Required(order = 6, messageResId = R.string.error_email)
+    @Email(order = 7, messageResId = R.string.error_email_valid)
     @ViewById
     EditText emailInput;
 
+    @Password(order = 8, messageResId = R.string.error_password)
+    @TextRule(order = 9, minLength = 6, messageResId = R.string.error_password_length)
     @ViewById
     EditText passwordInput;
+
+    @ViewById
+    Button nextButton;
+
+    @FragmentArg
+    String firstName;
+
+    @FragmentArg
+    String lastName;
+
+    @FragmentArg
+    String email;
 
     @AnimationRes
     Animation shake;
 
-
-    @FragmentArg String firstName;
-    @FragmentArg String lastName;
-    @FragmentArg String email;
-
     private boolean isInAfterTextChange;
     private Validator validator;
+    private AppMsg msg;
 
     @AfterViews
     public void onAfterViews() {
@@ -79,14 +99,35 @@ public class RegistrationInfoFragment extends ContractFragment<RegistrationFragm
         emailInput.setText(email);
     }
 
+
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onDialogDateSet(int reference, int y, int m, int d) {
+        setDateFromDatePicker(y, m, d);
     }
 
     @Override
-    public void onStop() {
-        super.onStop();
+    public void onValidationSucceeded() {
+        if (msg != null && msg.isShowing()) {
+            msg.cancel();
+        }
+        msg = AppMsg.makeText(getActivity(), "You did it!", AppMsg.STYLE_INFO);
+        msg.show();
+    }
+
+    @Override
+    public void onValidationFailed(View failedView, Rule<?> failedRule) {
+        if (failedView instanceof EditText) {
+            EditText editText = (EditText) failedView;
+            if (!editText.requestFocus()) {
+                editText.performClick();
+            }
+            editText.startAnimation(shake);
+        }
+        if (msg != null && msg.isShowing()) {
+            msg.cancel();
+        }
+        msg = AppMsg.makeText(getActivity(), failedRule.getFailureMessage(), AppMsg.STYLE_ALERT);
+        msg.show();
     }
 
     @Click
@@ -98,39 +139,11 @@ public class RegistrationInfoFragment extends ContractFragment<RegistrationFragm
         datePickerBuilder.show();
     }
 
-    public void setDateFromDatePicker(int y, int m, int d) {
-        String date = String.format("%02d/%02d/%4d", m + 1, d, y);
-        dobInput.setText(date);
-        phoneInput.requestFocus();
-    }
-
-
     @Click
     public void nextButton() {
         validator.validate();
-        /*
-        User user = new User();
-        user.setUsername(usernameInput.getText().toString());
-        user.setPassword(passwordInput.getText().toString());
-        user.setEmail(emailInput.getText().toString());
-        user.put(User.PHONE, phoneInput.getText().toString());
-        user.put(User.DATE_OF_BIRTH, dobInput.getText().toString());
-
-        final SitcProgressDialog progressDialog = new SitcProgressDialog(getActivity(), "Signing up");
-        progressDialog.show();
-        user.signUpInBackground(new SignUpCallback() {
-            @Override
-            public void done(ParseException e) {
-                progressDialog.dismiss();
-                if (e == null) {
-                    getContract().onRegistrationFragmentNext(RegistrationInfoFragment.this);
-                } else {
-                    Toast.makeText(getActivity(), "User save error", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        */
     }
+
 
     @AfterTextChange
     public void phoneInputAfterTextChanged(Editable text) {
@@ -143,23 +156,9 @@ public class RegistrationInfoFragment extends ContractFragment<RegistrationFragm
         }
     }
 
-    @Override
-    public void onDialogDateSet(int reference, int y, int m, int d) {
-        setDateFromDatePicker(y, m, d);
-    }
-
-    @Override
-    public void onValidationSucceeded() {
-
-    }
-
-    @Override
-    public void onValidationFailed(View failedView, Rule<?> failedRule) {
-        if (failedView instanceof EditText) {
-            EditText editText = (EditText) failedView;
-            editText.requestFocus();
-            editText.startAnimation(shake);
-        }
-        AppMsg.makeText(getActivity(), failedRule.getFailureMessage(), AppMsg.STYLE_ALERT).show();
+    public void setDateFromDatePicker(int y, int m, int d) {
+        String date = String.format("%02d/%02d/%4d", m + 1, d, y);
+        dobInput.setText(date);
+        phoneInput.requestFocus();
     }
 }
